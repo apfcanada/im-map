d5b439a8e89c4a22f98a0fde01fb2ee9af4f6583
Nate Wessel <bike756@gmail.com>
2020-08-13 14:16:08 -0400

    add geodesic (w bug)

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import { map, tileLayer } from 'leaflet'
+import { map, tileLayer, LatLng } from 'leaflet'
+import { GeodesicLine } from 'leaflet.geodesic'
+import { csv } from 'd3-fetch'
+
 
 const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 
@@ -6,8 +9,10 @@ const basemapStyle = 'apfcanada/ckcxiyprg0wwf1kqrmu48n937'
 
 const publicToken = 'pk.eyJ1IjoiYXBmY2FuYWRhIiwiYSI6ImNrY3hpdzcwbzAwZzIydms3NGRtZzY2eXIifQ.E7PbT0YGmjJjLiLmyRWSuw'
 
+var theMap
+
 window.onload = function () {
-	var mymap = map('map').setView([43.7,-79.5],10)
+	theMap = map('map').setView([43.7,-79.5],3)
 	
 	tileLayer(
 		'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
@@ -19,6 +24,21 @@ window.onload = function () {
 			'zoomOffset': -1,
 			'accessToken': publicToken
 		}
-	).addTo(mymap)
+	).addTo(theMap)
 
+	addSampleData()
+}
+
+function addSampleData(){
+	csv('./data/sample.csv').then( data => {
+		data.map( d => {
+			let orig = new LatLng(d.lat_from,d.lon_from)
+			let dest = new LatLng(d.lat_to,d.lon_to)
+			d.vector = new GeodesicLine(
+				[orig,dest], 
+				{ color: 'white', weight: 1, steps: 5, wrap: false }
+			).addTo(theMap)
+		} )
+		console.log( data )
+	} )
 }
